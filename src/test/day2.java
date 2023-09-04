62ea1f4b727f6ceebaf251d8fde9ee585c164e48
pratheepapreethi <epratheepa@gmail.com>
2023-09-04 22:17:17 +0530

    changes made american

--- src/test/day2.java
+++ src/test/day2.java
@@ -9,14 +9,20 @@ public class day2 {
 	@Test(groups={"Smoke"})
 	public void ploan()
 	{
-		System.out.println("good");
-		System.out.println("good2");
-		System.out.println("good3");
+		System.out.println(" execute last");
+		System.out.println("excecuted");
+	    System.out.println(" excecuted");
 	}
 	
 	@BeforeTest
 	public void prerequiste()
 	{
 		System.out.println("I will execute first");
+		System.out.println("I will execute last");
+		System.out.println("secondly excecuted");
+	    System.out.println("third excecuted");
+	
+	
+	
 	}
 }
